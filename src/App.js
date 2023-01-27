1467128ac82f2b0021fbcd9f78f27fc0bf9ad497
Rio Edwards <rioredwards@gmail.com>
2023-01-26 20:53:21 -0800

    Some final styling

--- src/App.js
+++ src/App.js
@@ -5,16 +5,10 @@ import GameBoard from './components/GameBoard.js';
 import Reset from './components/Reset.js';
 
 function App() {
-  const { gameBoard, currentPlayer, gameIsActive, gameMessage } = useContext(GameContext);
+  const { gameMessage } = useContext(GameContext);
 
   return (
     <div className="App">
-      <div className="context">
-        <div>{`gameBoard: ${gameBoard}`}</div>
-        <div>{`currentPlayer: ${currentPlayer}`}</div>
-        <div>{`gameIsActive: ${gameIsActive}`}</div>
-        <div>{`gameMessage: ${gameMessage}`}</div>
-      </div>
       <main>
         <div className="game-message">{gameMessage}</div>
         <Reset></Reset>
